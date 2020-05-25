8fcfcb57d45ca49e8a067984b667f19a1cdba7cc
Paco Rampas <pacorampas@gmail.com>
2020-05-25 16:43:11 +0200

    first

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,126 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {render} from 'react-dom'
 
-import Example from '../../src'
+import useCarousel, { useCarouselSwipe } from '../../src'
 
-class Demo extends Component {
-  render() {
-    return <div>
-      <h1>pr-carousel Demo</h1>
-      <Example/>
+const DemoCarousel = () => {
+  const [
+    idActive, 
+    indexActive, 
+    {
+      next,
+      nextDisabled
+    }, 
+    {
+      back,
+      backDisabled
+    }
+  ] = useCarousel({ 
+    slidesId: ['hola', 'como', 'estas'], 
+    activeId: 'como'
+  })
+
+  return (
+    <div>
+      <h1>indexActive: {indexActive}, idActive: {idActive}</h1>
+      <button onClick={back} disabled={backDisabled} >Back</button>
+      <button onClick={next} disabled={nextDisabled}>Next</button>
     </div>
-  }
+  )
+}
+
+const DemoSwipe = () => {
+  const [
+    ref,
+    swipeProps,
+    deltaX,
+    deltaXPerc,
+    isSwiping
+  ] = useCarouselSwipe({ 
+    next: () => { console.log('next') }, 
+    back: () => { console.log('back') }
+  })
+
+  return (
+    <div
+      style={{
+        width: '500px',
+        height: '500px',
+        border: 'solid 1px #333'
+      }}
+      {...swipeProps}
+      ref={ref}
+    >
+      <div>DeltaX: {deltaX}</div>
+      <div>deltaXPerc: {deltaXPerc}</div>
+      <div>isSwipping: {isSwiping ? 'true' : 'false'}</div>
+    </div>
+  )
+}
+
+const DemoCarouselSwipe = () => {
+  const [
+    idActive, 
+    indexActive, 
+    {
+      next,
+      nextDisabled
+    }, 
+    {
+      back,
+      backDisabled
+    }
+  ] = useCarousel({ 
+    slidesId: ['hola', 'como', 'estas'], 
+    activeId: 'como'
+  })
+
+  const [
+    ref,
+    swipeProps,
+    deltaX,
+    deltaXPerc,
+    isSwiping
+  ] = useCarouselSwipe({ 
+    next, 
+    back
+  })
+
+  return (
+    <div>
+      <h1>indexActive: {indexActive}, idActive: {idActive}</h1>
+      <button onClick={back} disabled={backDisabled} >Back</button>
+      <button onClick={next} disabled={nextDisabled}>Next</button>
+      <div
+        style={{
+          width: '500px',
+          height: '500px',
+          border: 'solid 1px #333'
+        }}
+        {...swipeProps}
+        ref={ref}
+      >
+        <div>DeltaX: {deltaX}</div>
+        <div>deltaXPerc: {deltaXPerc}</div>
+        <div>isSwipping: {isSwiping ? 'true' : 'false'}</div>
+      </div>
+    </div>
+  )
+}
+
+const Demo = () => {
+  return (
+    <div>
+      demo1:
+      <DemoCarousel />
+      <hr />
+      demo2: 
+      <DemoSwipe />
+      <hr />
+      demo3: 
+      <DemoCarouselSwipe />
+    </div>
+  )
 }
 
 render(<Demo/>, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,9 +1,124 @@
-import React, {Component} from 'react'
+import React, { useEffect, useState, useCallback, useRef } from 'react'
+import { useSwipeable } from 'react-swipeable'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+function useCarousel({ slidesId, activeId }) {
+  const [idActiveInternal, setIdActiveInternal] = useState(activeId)
+  const [indexActiveInternal, setIndexActiveInternal] = useState()
+  const [nextDisabled, setNextDisabled] = useState(true)
+  const [backDisabled, setBackDisabled] = useState(true)
+  
+  const getIndexItemById = useCallback(id => {
+    const index = slidesId.findIndex(_id => _id === id)
+    return index
+  }, [slidesId])
+
+  const handleClickBack = useCallback(() => {
+    const index = getIndexItemById(idActiveInternal)
+    if (index === 0) {
+      return
+    }
+    
+    const nextIndex = index - 1
+    const nextActive = slidesId[nextIndex]
+    setIdActiveInternal(nextActive)
+  }, [idActiveInternal, slidesId, getIndexItemById])
+
+  const handleClickNext = useCallback(() => {
+    const index = getIndexItemById(idActiveInternal)
+
+    if (index === slidesId.length - 1) {
+      return
+    }
+
+    const nextIndex = index + 1
+    const nextActive = slidesId[nextIndex]
+    setIdActiveInternal(nextActive)
+  }, [idActiveInternal, slidesId, getIndexItemById])
+
+  useEffect(() => {
+    setIdActiveInternal(activeId)
+  }, [activeId])
+
+  useEffect(() => {
+    const indexActive = slidesId.findIndex(id => id === idActiveInternal)
+    setIndexActiveInternal(indexActive)
+    setNextDisabled(indexActive === slidesId.length - 1)
+    setBackDisabled(indexActive === 0)
+  }, [slidesId, idActiveInternal])
+
+  return [
+    idActiveInternal,
+    indexActiveInternal, 
+    {
+      next: handleClickNext,
+      nextDisabled
+    }, {
+      back: handleClickBack,
+      backDisabled
+    }
+  ]
+}
+
+export default useCarousel
+
+export function useCarouselSwipe({ back, next }) {
+ 
+  const [deltaX, setDeltaX] = useState(0)
+  const [deltaXPerc, setDeltaXPerc] = useState(0)
+  const [isSwiping, setSwiping] = useState(false)
+  const [width, setWidth] = useState()
+  const ref = useRef()
+
+  useEffect(() => {
+    console.log('hola', ref.current)
+    setWidth(ref.current.offsetWidth)
+  }, [])
+
+  const handleSwiped = ({ dir }) => {
+    setSwiping(false)
+    handleSwipeNext(dir)
   }
+
+
+  const hanldeSwiping = ({ deltaX }) => {
+    setDeltaX(deltaX)
+    setDeltaXPerc(deltaX / width * 100)
+    setSwiping(true)
+  }
+
+  const swipeProps = useSwipeable({ 
+    onSwiped: handleSwiped,
+    onSwiping: hanldeSwiping,
+    trackMouse: true
+  })
+
+
+  const handleSwipeNext = useCallback(dir => {
+    if ( !(deltaXPerc > 20 || deltaXPerc < -20) ) {
+      setDeltaXPerc(0)
+      return
+    }
+
+    // eslint-disable-next-line default-case
+    switch(dir) {
+      case 'Right':
+        back()
+        setDeltaXPerc(0)
+        setDeltaX(0)
+        return
+      case 'Left':
+        next()
+        setDeltaXPerc(0)
+        setDeltaX(0)
+        return
+    }
+  }, [deltaXPerc, next, back])
+
+  return [
+    ref,
+    swipeProps,
+    deltaX,
+    deltaXPerc,
+    isSwiping
+  ]
 }
